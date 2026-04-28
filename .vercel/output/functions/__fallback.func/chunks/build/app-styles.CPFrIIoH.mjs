3f24a129c33eb1158330ac77fa7f8f0e618c4c65
ali203495 <abdelaali.markabi@gmail.com>
2026-04-28 16:16:43 +0100

    .

---
+++ .vercel/output/functions/__fallback.func/chunks/build/app-styles.CPFrIIoH.mjs
@@ -0,0 +1,2 @@
+const e=[".page-enter-active,.page-leave-active{transition:all .4s}.page-enter-from,.page-leave-to{filter:blur(1rem);opacity:0}"];export{e as default};
+//# sourceMappingURL=app-styles.CPFrIIoH.mjs.map
