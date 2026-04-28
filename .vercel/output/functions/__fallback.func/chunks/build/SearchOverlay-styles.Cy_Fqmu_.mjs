6422c3756a65ee41ea1ca84ddb6c2051c46ce5b1
ali203495 <abdelaali.markabi@gmail.com>
2026-04-28 18:36:20 +0100

    fix 500 crash (remove debug scripts)

--- .vercel/output/functions/__fallback.func/chunks/build/SearchOverlay-styles.Cy_Fqmu_.mjs
+++
@@ -1,2 +0,0 @@
-const a=[".fade-enter-active[data-v-6d8be9d5],.fade-leave-active[data-v-6d8be9d5]{transition:opacity .3s ease}.fade-enter-from[data-v-6d8be9d5],.fade-leave-to[data-v-6d8be9d5]{opacity:0}"];export{a as default};
-//# sourceMappingURL=SearchOverlay-styles.Cy_Fqmu_.mjs.map
--- .vercel/output/functions/__fallback.func/chunks/build/tracking-styles.k0Si1zz2.mjs
+++
@@ -1,2 +0,0 @@
-const a=[".fade-enter-active[data-v-dd3cfb5d],.fade-leave-active[data-v-dd3cfb5d]{transition:opacity .5s ease}.fade-enter-from[data-v-dd3cfb5d],.fade-leave-to[data-v-dd3cfb5d]{opacity:0}"];export{a as default};
-//# sourceMappingURL=tracking-styles.k0Si1zz2.mjs.map
